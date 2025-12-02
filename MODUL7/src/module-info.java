5d4ad91d24b409ff60e04f6e2e5368c93c0b2184
Mullet11 <rakha1140athallah@gmail.com>
2025-12-02 21:21:25 +0800

    Delete MODUL7 directory

--- MODUL7/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module MODUL7 {
-	requires javafx.controls;
-	requires java.sql;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
-	opens controller to javafx.fxml;
-	opens model to javafx.base;
-}
